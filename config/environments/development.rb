5e03669dc3f534a6e42a91cb392e5b639f022573
mikemunevar <mikemunevar@gmail.com>
2016-09-18 18:13:50 +0000

    Implemented gemfiles for handling emailing in both development and production.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,10 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # Send emails using letter_opener in dev mode to open email in a browser #MDM
+  config.action_mailer.delivery_method = letter_opener
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
